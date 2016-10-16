ab96b0ff7ed42e06874c1dc4fd78f9e8299bb298
JoFu2 <tgointel@gmail.com>
2016-10-16 00:31:59 -0400

    Initial Commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+    public float moveSpeed;
+    public float turnSpeed;
+    public Text mouseText;
+    public Text playerText;
+    private Rigidbody rb;
+
+    private Vector3 inputMovement;
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        //inputMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //float diagonalFactor = Input.GetAxis("Horizontal") != 0 && Input.GetAxis("Vertical") != 0 ? diagonalFactor = 1f / Mathf.Sqrt(2.0f) : 1;
+        //transform.Translate(inputMovement * Time.deltaTime * moveSpeed * diagonalFactor, Space.World);
+        //transform.Rotate(new Vector3(0, 10) * Time.deltaTime * turnSpeed);
+        mouseText.text = "Mouse Pos: " + Input.mousePosition.ToString();
+        playerText.text = "Player Pos: " + transform.position.ToString();
+    }
+
+    void FixedUpdate()
+    {
+        Quaternion rotation = rb.rotation;
+        rotation[0] = 0;
+        rotation[2] = 0;
+        rb.rotation = rotation;
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * moveSpeed);        
+    }
+}
